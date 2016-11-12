bc760e59296500b690ba9942f2d7666498d078f1
mkaminsky11 <mkaminsky11@gmail.com>
2016-11-12 02:45:42 -0500

    get started with text manipulations

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
-import serial
-ser = serial.Serial('/dev/ttyACM0', 9600)
+#import serial
+#ser = serial.Serial('/dev/ttyACM0', 115200)
 
 # ser.write('3')
 # row | col on|off
@@ -23,26 +23,46 @@ cols = 540 # cutoffs: 0-179 180-359 360-539
 # }
 #
 
-def diff(newState):
+#def diff(newState):
 	#
 
-def unwrap(text):
+#def unwrap(text):
 	#
+
+def arrayToSingleBlock(data):
+	text = ""
+	for i in range(0,7):
+		for item in data:
+			text = text + item[i]
+		text = text + "\n"
+	return text;
 
 def textToFont(text):
-	#
+	ret = []
+	for char in text:
+		if char in ledFont:
+			# :)
+			ret.append(ledFont[char])
+		else:
+			if char == " ":
+				ret.append(ledFont_space)
+	return ret
+
 
 led = open('normalfont','r').read().strip().split("\n")
 ledFont = {}
 for line in led:
 	line = line.split(" ")
 	ledFont[line[0]] = line[1::]
-
-print(ledFont)
+ledFont_space = ["000","000","000","000","000","000","000"]
 
 current_state = []
-for i in rows:
+default_state = []
+for i in range(0,rows):
 	row = []
-	for j in cols:
+	for j in range(0,cols):
 		row.append(False)
 	current_state.append(row)
+	default_state.append(row)
+
+print(arrayToSingleBlock(textToFont("Hello world!")))

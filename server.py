b5949b866d3d51db06ca45692132d3eb6a5e4115
mkaminsky11 <mkaminsky11@gmail.com>
2016-11-27 06:35:46 +0000

    changes

--- server.py
+++ server.py
@@ -1,3 +1,25 @@
+#
+#
+# FLASK
+#
+#
+#
+#
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+#
+#
+#
+# THE REAL STUFF
+#
+#
+
 from time import sleep
 import sys
 import time

d1f7b55b41a0216260c314d15f7dc609816263b1
mkaminsky11 <mkaminsky11@gmail.com>
2016-11-27 22:26:45 -0500

    ip address stuff

--- ip.py
+++ ip.py
@@ -4,9 +4,8 @@ def get():
 	ret = subprocess.Popen(['hostname','-I'], stdin=subprocess.PIPE, stdout=subprocess.PIPE).communicate()[0].strip().decode().split(" ")
 	#ret = subprocess.Popen(['ifconfig','wlan1','|','grep','"inet','addr:"'])
 	#-> inet addr:18.111.92.166  Bcast:18.111.95.255  Mask:255.255.224.0
-	_ip = "127.0.0.1"	
+	_ip = []
 	for r in ret:
 		if "18" in r: # mit ip address and stuff
-			return r
-			#_ip = r
-	return _ip
+			_ip.append(r)
+	return " ".join(ip)
